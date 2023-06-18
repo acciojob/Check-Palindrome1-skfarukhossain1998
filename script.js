be613802df6d217060ec418b256f59536984cef8
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-06-18 17:27:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
+function isPalindrome(s) {
+  // Convert the string to lowercase and remove non-alphanumeric characters
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-function palindrome(str){
+  // Reverse the string
+  const reversed = s.split('').reverse().join('');
 
+  // Compare the original string with the reversed string
+  return s === reversed;
 }
-module.exports = palindrome
+
+// Example usage
+const inputString = "race a car";
+console.log(isPalindrome(inputString));
+
+module.exports = isPalindrome
